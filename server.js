cb263bed94a916ef365aa38864c6006fbe0ced8c
JonesVh <jonesveriato@gmail.com>
2020-08-17 10:38:29 -0300

    deploy

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import path from 'path';
 const port = process.env.PORT || 3000;
 const app = express();
 
+__dirname = path.resolve();
+
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
