f8580bc18ba2ac770f49092a1cee1ebeed3dcd50
JonesVh <jonesveriato@gmail.com>
2020-08-17 08:58:22 -0300

    deploy

--- server.ts
+++ server.ts
@@ -18,3 +18,5 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port, ()=>{console.log(`listen port ${port}`)});
+
+export default undefined
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="react-scripts" />
+
+export default undefined
