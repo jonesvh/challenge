ddb39c83ec26c086027efb748219bb55761c34ea
JonesVh <jonesveriato@gmail.com>
2020-08-17 00:59:57 -0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { } from 'react';
-import GlobalStyle from './Styles/global';
+import GlobalStyle from './global';
 import Routes from './Routes';
 import LeadsProvider from './Context/Leads';
 
