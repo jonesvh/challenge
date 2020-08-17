3867eed1aca6744bec2a05bc4d507ff9142abdd8
JonesVh <jonesveriato@gmail.com>
2020-08-17 01:31:39 -0300

    deploy

---
+++ build/precache-manifest.c86044070a419d42e74284d1ceb4a1b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e6b66a8f8e63d9a521ca59455d3ab8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "821a75e05df111cb4394",
+    "url": "/static/js/2.08282bf2.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.08282bf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa8a20a19c7a51c0eeff",
+    "url": "/static/js/main.1763ccf5.chunk.js"
+  },
+  {
+    "revision": "b66c89724249238f765b",
+    "url": "/static/js/runtime-main.64472567.js"
+  }
+]);
