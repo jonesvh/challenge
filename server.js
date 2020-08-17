3bc25a9fa830d58239c0d13de338464d8aa31c53
JonesVh <jonesveriato@gmail.com>
2020-08-17 01:22:55 -0300

    deploy

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
-app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
-    if (req.headers["x-forwarded-proto"] == "http") //Checa se o protocolo informado nos headers é HTTP
-        res.redirect(`https://${req.hostname}${req.url}`); //Redireciona pra HTTPS
-    else //Se a requisição já é HTTPS
-        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
-});
+
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
@@ -19,4 +14,4 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port, ()=>{console.log('listen port 3000')});
+app.listen(port, ()=>{console.log(`listen port ${port}`)});
